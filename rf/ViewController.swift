37a2767061d900cac5ffbb76f89f21cfc3c072c5
sunnyatic <sunnyatic@yahoo.com>
2020-10-30 15:37:00 -0700

    Sunny's changes 2

--- rf/ViewController.swift
+++ rf/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Hello new World")
         print("Sunny's changes")
+        print("This is the end of October")
         // Do any additional setup after loading the view.
     }
 
